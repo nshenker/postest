c11573d11c498b096e7d69af93ad45cda26115ff
nshenker <64337739+nshenker@users.noreply.github.com>
2023-03-26 04:33:44 -0400

    Update stores.js

--- src/routes/stores.js
+++ src/routes/stores.js
@@ -11,6 +11,12 @@ export const mints = writable([
     {name:"USDC", mint:"EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"},
 
     {name:"DUST", mint:"DUSTawucrTsGU8hcqRdHDCbuYhCPADMLM2VcCb8VnFnQ"},
+
+    {name:"FOXY", mint:"FoXyMu5xwXre7zEoSvzViRk3nGawHUp9kUh97y2NDhcq"},
+
+    {name:"JOINTS", mint:"7ftKSttU6yUAnWsWxpRP3LKdQNEto8V4KD9NuWttoVnV"},
+
+    {name:"USDT", mint:"Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB"},
  
     {name:"BONK", mint:"DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263"}])
 export const selectedMint = writable("USDC");
